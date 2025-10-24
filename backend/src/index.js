192a3d52f0fdfb382657205543e1770742f18eb2
Jonatan Hyberg <jonatan.hyberg@hotmail.com>
2025-10-24 20:41:05 +0200

    Added get request to server

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,6 +8,31 @@ app.use(express.json())
 
 const PORT = 3001
 
+let todos = {
+      '0000000001': {
+        id: '0000000001',
+        title: 'First List',
+        todos: ['First todo of first list!'],
+      },
+      '0000000002': {
+        id: '0000000002',
+        title: 'Second List',
+        todos: ['First todo of second list!'],
+      },
+    };
+
 app.get('/', (req, res) => res.send('Hello World!'))
+
+//fetch todo list
+app.get('/api/todos', (req, res) => {
+    res.json(todos);
+});
+
+app.post('/api/message', (req, res) => {
+    console.log(req.body.text)
+    const newTodo = {id: todos.length() + 1, title: "second Todo list", todo: []};
+    todos.push(newTodo)
+    res.status(201).json(newTodo)
+});
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
